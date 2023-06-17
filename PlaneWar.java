75fe18da2404a1349b97fe4c18e8443ee89040a6
Lry722 <958515687@qq.com>
2023-06-18 00:33:41 +0800

    20230618-00:33:41

--- PlaneWar.java
+++ PlaneWar.java
@@ -6,8 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class PlaneWar extends JFrame implements ActionListener {
